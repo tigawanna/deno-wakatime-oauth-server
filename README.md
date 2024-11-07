3453b732ca9791ca80fedfad200ad8b84f5117d4
tigawanna <denniskinuthiaw@gmail.com>
2024-11-07 08:47:18 +0300

    Enhance Deno setup with dotenv support and environment variable checks

--- README.md
+++ README.md
@@ -1,3 +1,122 @@
+Wakatime enpoints
+
+Oauth to authorize and get token 
+```ts
+  const wakatime_oauth_endoint = "https://wakatime.com/oauth/authorize ";
+
+  const response_type = "token";
+  const redirect_uri = `${my_app}/redirect`;
+  const scope = "read_stats,read_summaries";
+
+  const wakatimeAuthUrl = new URL(wakatime_oauth_endoint);
+  wakatimeAuthUrl.searchParams.append("client_id", client_id);
+  wakatimeAuthUrl.searchParams.append("response_type", response_type);
+  wakatimeAuthUrl.searchParams.append("redirect_uri", redirect_uri);
+  wakatimeAuthUrl.searchParams.append("scope", scope);
+  // redirect to this url and get the code in the url and pass it to the token api
+
+  //  response wil look something like 
+
+  // https://6beeaf9bbae3cc.lhr.life/redirect#access_token=waka_tok_jzfbdhN8Jn9Gn5NZEjuHAnNqHyquj03FVrWOEomqyZmqCq22P&expires_at=2024-11-07T15%3A03%3A39Z&expires_in=432002&4e4refresh_token=waka_ref_5nVncyobrrqrr9dzscope=read_summaries&state=STATE&token_type=bearer&uid=018cc20e-ad5a-4d40-ad76-7d398731446f
+
+type ResponseParams = {
+    access_token:string;
+    expires_at:string;
+    expires_in:number;
+    refresh_token:string;
+    scope:string;
+    state:string;
+    token_type:string;
+    uid:string
+  }
 ```
-deno task start
+
+
+get current user 
+
+```ts
+  const response =  await fetch("https://wakatime.com/api/v1/users/current",{
+    headers: {
+      "Authorization":"Bearer waka_tok_your_token"
+    }
+  })
+
+
+export interface Response {
+    data: Data;
+}
+
+export interface Data {
+    id:                        string;
+    email:                     string;
+    timezone:                  string;
+    timeout:                   number;
+    writes_only:               boolean;
+    created_at:                Date;
+    location:                  null;
+    logged_time_public:        boolean;
+    needs_payment_method:      boolean;
+    languages_used_public:     boolean;
+    is_onboarding_finished:    boolean;
+    show_machine_name_ip:      boolean;
+    profile_url:               string;
+    time_format_24hr:          null;
+    date_format:               string;
+    share_last_year_days:      null;
+    city:                      null;
+    twitter_username:          null;
+    plan:                      string;
+    time_format_display:       string;
+    has_basic_features:        boolean;
+    username:                  null;
+    share_all_time_badge:      null;
+    last_plugin:               string;
+    is_email_confirmed:        boolean;
+    public_profile_time_range: string;
+    public_email:              null;
+    invoice_id_format:         string;
+    last_project:              string;
+    full_name:                 null;
+    color_scheme:              string;
+    linkedin_username:         null;
+    photo:                     string;
+    last_heartbeat_at:         Date;
+    human_readable_website:    null;
+    website:                   null;
+    photo_public:              boolean;
+    default_dashboard_range:   string;
+    wonderfuldev_username:     null;
+    display_name:              string;
+    has_premium_features:      boolean;
+    weekday_start:             number;
+    is_email_public:           boolean;
+    durations_slice_by:        string;
+    modified_at:               Date;
+    last_plugin_name:          string;
+    is_hireable:               boolean;
+    last_language:             string;
+    last_branch:               string;
+    profile_url_escaped:       string;
+    github_username:           null;
+    bio:                       null;
+    meetings_over_coding:      boolean;
+}
+
+
+```
+how to use access token 
+>[!NOTE]
+> access token can either be passed in the url params or in the headers
+
+```ts
+  const summariesEndpoint = new URL("https://wakatime.com/api/v1/users/current/summaries"); 
+   if(temp_access_token){
+     summariesEndpoint.searchParams.append("access_token", temp_access_token);
+   }
+   summariesEndpoint.searchParams.append("start", "11/01/2024");
+   summariesEndpoint.searchParams.append("end", "11/02/2024");
+  const response =  await fetch(summariesEndpoint.toString(),{
+    headers: {
+      "Authorization":"Bearer "+temp_access_token
+  }})
 ```
--- main.ts
+++ main.ts
@@ -1,82 +1,94 @@
-import { Hono } from 'hono'
+import "jsr:@std/dotenv/load";
+import { Hono } from "hono";
 
-const app = new Hono({})
+const app = new Hono({});
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
-app.post('/oauth/token', async(c) => {
-  console.log(" ==== c.req.url  ==== ",c.req.url)
-//   https://wakatime.com/oauth/token - Make a server-side POST request here to get the secret access token. Required data is client_id, client_secret, redirect_uri must be the same url used in authorize step, grant_type of authorization_code, and the code received from the authorize step.
-  
-const wakatime_oauth_endoint = "https://wakatime.com/oauth/token "
-  
-  const redirect_uri = `${my_app}/redirect`
+const my_app = Deno.env.get("MY_APP");
+const client_id = Deno.env.get("CLIENT_ID");
+const client_secret = Deno.env.get("CLIENT_SECRET");
+const temp_access_token = Deno.env.get("TEMP_ACCESS_TOKEN");
 
+if (!my_app) {
+  throw new Error("MY_APP is not set");
+}
+if (!client_id) {
+  throw new Error("CLIENT_ID is not set");
+}
+if (!client_secret) {
+  throw new Error("CLIENT_SECRET is not set");
+}
 
-  const wakatimeAuthUrl = new URL(wakatime_oauth_endoint)
-  wakatimeAuthUrl.searchParams.append('client_id', client_id)
-  wakatimeAuthUrl.searchParams.append('client_secret', client_secret)
-  wakatimeAuthUrl.searchParams.append('redirect_uri', redirect_uri)
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+app.post("/oauth/token", async (c) => {
+  console.log(" ==== c.req.url  ==== ", c.req.url);
+  //   https://wakatime.com/oauth/token - Make a server-side POST request here to get the secret access token. Required data is client_id, client_secret, redirect_uri must be the same url used in authorize step, grant_type of authorization_code, and the code received from the authorize step.
+  const wakatime_oauth_endoint = "https://wakatime.com/oauth/token ";
+  const redirect_uri = `${my_app}/redirect`;
+
+  const wakatimeAuthUrl = new URL(wakatime_oauth_endoint);
+  wakatimeAuthUrl.searchParams.append("client_id", client_id);
+  wakatimeAuthUrl.searchParams.append("client_secret", client_secret);
+  wakatimeAuthUrl.searchParams.append("redirect_uri", redirect_uri);
   try {
-  const resopnse =  await fetch(wakatimeAuthUrl.toString(), {
-      method: 'POST',
+    const resopnse = await fetch(wakatimeAuthUrl.toString(), {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        'grant_type': 'authorization_code',
-        'code': 'code'
-      })
-    }).then((r)=>{
-      if(!r.ok){
-        console.log(" ==== resopnse  ==== ",r)
+        "grant_type": "authorization_code",
+        "code": "code",
+      }),
+    }).then((r) => {
+      if (!r.ok) {
+        console.log(" ==== resopnse  ==== ", r);
         return c.json({
           status: r.status,
           statusText: r.statusText,
-        })
+        });
       }
-      console.log(" ==== resopnse  ==== ",r)
-      return r
-    })
+      console.log(" ==== resopnse  ==== ", r);
+      return r;
+    });
     return c.json({
       status: 200,
-      resopnse
-    })
-  } catch (error:any) {
-    console.log(" ==== error . message  ==== ",error.message)
+      resopnse,
+    });
+  } catch (error: any) {
+    console.log(" ==== error . message  ==== ", error.message);
     return c.json({
       status: 500,
-      error: error.message
-    })
+      error: error.message,
+    });
   }
 
-
   // return c.redirect(wakatimeAuthUrl.toString())
-})
-app.post('/oauth/authorize', (c) => {
-  // https://wakatime.com/oauth/authorize - 
-  // Redirect your users here to request permission to access their account. 
+});
+app.post("/oauth/authorize", (c) => {
+  // https://wakatime.com/oauth/authorize -
+  // Redirect your users here to request permission to access their account.
   // Required url arguments are client_id, response_type of code or token, redirect_
   // uri. Optional parameters are scope, state, force_approve.
-  const wakatime_oauth_endoint = "https://wakatime.com/oauth/authorize "
-  
-  const response_type = "token"
-  const redirect_uri = `${my_app}/redirect`
-  const scope = "read_stats,read_summaries"
-  const state = "STATE"
-  const force_approve = "false"
+  const wakatime_oauth_endoint = "https://wakatime.com/oauth/authorize ";
 
-  const wakatimeAuthUrl = new URL(wakatime_oauth_endoint)
-  wakatimeAuthUrl.searchParams.append('client_id', client_id)
-  wakatimeAuthUrl.searchParams.append('response_type', response_type)
-  wakatimeAuthUrl.searchParams.append('redirect_uri', redirect_uri)
-  wakatimeAuthUrl.searchParams.append('scope', scope)
-  wakatimeAuthUrl.searchParams.append('state', state)
-  wakatimeAuthUrl.searchParams.append('force_approve', force_approve)
-  console.log("wakatime url  == ", wakatimeAuthUrl.toString())
-  return c.redirect(wakatimeAuthUrl.toString())
-})
+  const response_type = "token";
+  const redirect_uri = `${my_app}/redirect`;
+  const scope = "read_stats,read_summaries";
+  const state = "STATE";
+  const force_approve = "false";
+
+  const wakatimeAuthUrl = new URL(wakatime_oauth_endoint);
+  wakatimeAuthUrl.searchParams.append("client_id", client_id);
+  wakatimeAuthUrl.searchParams.append("response_type", response_type);
+  wakatimeAuthUrl.searchParams.append("redirect_uri", redirect_uri);
+  wakatimeAuthUrl.searchParams.append("scope", scope);
+  wakatimeAuthUrl.searchParams.append("state", state);
+  wakatimeAuthUrl.searchParams.append("force_approve", force_approve);
+  console.log("wakatime url  == ", wakatimeAuthUrl.toString());
+  return c.redirect(wakatimeAuthUrl.toString());
+});
 // app.post('/redirect', async(c) => {
 //   const responseUrl = c.req.url
 //   const responseBody =    await c.req.parseBody()
@@ -88,47 +100,82 @@ app.post('/oauth/authorize', (c) => {
 //     responseBody
 //   })
 // })
-app.get('/redirect', async (c) => {
-  const responseUrl = c.req.url
-  const responseBody = await c.req.parseBody()
-  console.log("responseUrl == ", responseUrl)
-  console.log("responseBody == ", responseBody)
+app.get("/redirect", async (c) => {
+  const responseUrl = c.req.url;
+  const responseBody = await c.req.parseBody();
+  console.log("responseUrl == ", responseUrl);
+  console.log("responseBody == ", responseBody);
 
   return c.json({
     responseUrl,
-    responseBody
+    responseBody,
+  });
+});
+
+
+app.get("/summaries", async(c) => {
+  try{
+  const summariesEndpoint = new URL("https://wakatime.com/api/v1/users/current/summaries"); 
+   if(temp_access_token){
+     // summariesEndpoint.searchParams.append("access_token", temp_access_token);
+   }
+   summariesEndpoint.searchParams.append("start", "11/01/2024");
+   summariesEndpoint.searchParams.append("end", "11/02/2024");
+  const response =  await fetch(summariesEndpoint.toString(),{
+    headers: {
+      "Authorization":"Bearer "+temp_access_token
+  }})
+  .then((r) => {
+  if(!r.ok){
+    return c.json({
+      status: r.status,
+      statusText: r.statusText
+    })
+  }
+  return  r.json()
   })
-})
+  return c.json({
+    status: 200,
+    response
+  })}
+  catch (error: any) {
+    console.log(" ==== error . message  ==== ", error.message);
+    return c.json({
+      status: 500,
+      error: error.message,
+    });
+  }
+});
+// access_token = waka_tok_jzfbdhOWu8YVkLmGN8Jn9tButM6Gn5NZEjuHAnNqHDNdPJRY6JyquKxIX4dj03FVrWOEomqyZmqCq22P & start=11 /01 / 2024 & end=11 /02 / 2024
+app.get("/current", async(c) => {
+try{
+  const response =  await fetch("https://wakatime.com/api/v1/users/current",{
+    headers: {
+      "Authorization":"Bearer waka_tok_jzfbdhOWu8YVkLmGN8Jn9tButM6Gn5NZEjuHAnNqHDNdPJRY6JyquKxIX4dj03FVrWOEomqyZmqCq22P"
+    }
+  })
+  .then((r) => {
+  if(!r.ok){
+    return c.json({
+      status: r.status,
+      statusText: r.statusText
+    })
+  }
+  return  r.json()
+  })
+  return c.json({
+    status: 200,
+    response
+  })}
+  catch (error: any) {
+    console.log(" ==== error . message  ==== ", error.message);
+    return c.json({
+      status: 500,
+      error: error.message,
+    });
+  }
+});
 
-Deno.serve({ port: 5000 }, app.fetch)
+Deno.serve({ port: 5000 }, app.fetch);
 
 
-
-// Scopes
-// Scopes are sent to the authorize url as a comma or space separated list, to request optional permissions for a user.
-
-//   read_summaries - access user’s Summaries and Stats including categories, dependencies, editors, languages, machines, operating systems, and projects.Consider instead requesting scopes for only the summaries you need.For ex: scope = read_summaries.languages, read_summaries.editors to only request access to the user’s language and editor summaries and stats.
-//     read_summaries.categories - access user’s Summaries and Stats, limited to the user’s categories.
-//       read_summaries.dependencies - access user’s Summaries and Stats, limited to the user’s dependencies.
-//         read_summaries.editors - access user’s Summaries and Stats, limited to the user’s editors.
-//           read_summaries.languages - access user’s Summaries and Stats, limited to the user’s languages.
-//             read_summaries.machines - access user’s Summaries and Stats, limited to the user’s machines.
-//               read_summaries.operating_systems - access user’s Summaries and Stats, limited to the user’s operating systems.
-//                 read_summaries.projects - access user’s Summaries and Stats, limited to the user’s projects.
-//                   read_stats - access user’s Stats including categories, dependencies, editors, languages, machines, operating systems, and projects.Consider instead requesting scopes for only the stats you need.For ex: scope = read_stats.languages, read_stats.editors to only request access to the user’s language and editor stats.
-//                     read_stats.best_day - access user’s Stats, limited to the user’s best day in the requested time range.
-//                       read_stats.categories - access user’s Stats, limited to the user’s categories.
-//                         read_stats.dependencies - access user’s Stats, limited to the user’s dependencies.
-//                           read_stats.editors - access user’s Stats, limited to the user’s editors.
-//                             read_stats.languages - access user’s Stats, limited to the user’s languages.
-//                               read_stats.machines - access user’s Stats, limited to the user’s machines.
-//                                 read_stats.operating_systems - access user’s Stats, limited to the user’s operating systems.
-//                                   read_stats.projects - access user’s Stats, limited to the user’s projects.
-//                                     read_goals - access user’s Goals.
-//                                       read_orgs - access user’s organizations, and coding activity for dashboard members.
-//                                         write_orgs - modify user’s organizations, and org dashboards.
-//                                           read_private_leaderboards - access user’s private leaderboards.
-//                                             write_private_leaderboards - modify user’s private leaderboards, including adding / removing members when current user had Admin or Owner role.
-//                                               read_heartbeats - access user’s coding activity, projects, files, editors, languages, operating systems, dependencies, Stats, Durations, External Durations, and Heartbeats.If you don’t need access to Durations or Heartbeats, consider only requesting read_summaries scope instead.
-//                                                 write_heartbeats - modify user’s coding activity with the ability to create, edit, and delete Heartbeats and External Durations.
-//                                                   email - access user’s private email address; not necessary for user’s public email and public profile info.
